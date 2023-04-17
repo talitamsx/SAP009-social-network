dc0182dc34d68737ece65cf728ae93a3fed364ce
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-04-17 17:12:39 -0300

    continuaçao-sobre-nos

--- src/pages/sobre/sobre.js
+++ src/pages/sobre/sobre.js
@@ -9,13 +9,12 @@ export default () => {
      <h2 class="subtitulo">Incentivando a leitura feminista</h2> 
     </div>
   </header>
-  
-  <h2>A Lemos é uma rede social que surgiu a partir da identificação da 
-  vontade das mulheres em aumentar ou iniciar um repertório de leitura com
-  temática feminista. Pensando nisso, desenvolvemos esse projeto para
-  publicar informações pontuais e comentários que facilitem a divulgação 
-  de livros, autoras e também indiquem o nível de leitura: inciante, 
-  intermediário ou avançado.</h2>
+  <h2 class="sobre">A Lemos é uma rede social que surgiu a partir da 
+  identificação da vontade das mulheres em aumentar ou iniciar um 
+  repertório de leitura com temática feminista. Pensando nisso, 
+  desenvolvemos esse projeto para publicar informações pontuais e 
+  comentários que facilitem a divulgação de livros, autoras e também 
+  indiquem o nível de leitura: inciante, intermediário ou avançado.</h2>
   `;
 
   container.innerHTML = template;
